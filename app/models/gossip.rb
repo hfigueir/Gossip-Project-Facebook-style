f5c07e30000cabbbb345bc84bc55fa0d5e8adfe4
hfigueir <hugoscp@hotmail.fr>
2018-11-01 14:27:18 +0100

    h

--- app/models/gossip.rb
+++
@@ -1,4 +0,0 @@
-class Gossip < ApplicationRecord
-end
-
-#mon model Gossip, qui contient la classe gossip et qui lui permettra d'interpréter les actions du controller, et de contenir la donnée adossée
